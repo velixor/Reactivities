69be52e215a500f25aafb0bddb6cebadc5f74f15
Pavel Dushkov <p.dushkov@dextechnology.com>
2020-03-02 19:18:55 +0200

    Init commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+        }
+
+        [HttpGet, PublicAPI]
+        public async Task<ActionResult<IEnumerable<Value>>> GetAsync()
+        {
+            var values = await _dataContext.Values.ToListAsync().ConfigureAwait(false);
+            return Ok(values);
+        }
+
+        [HttpGet("{id}"), PublicAPI]
+        public async Task<ActionResult<Value>> GetActionAsync(int id)
+        {
+            var value = await _dataContext.Values.SingleAsync(x => x.Id == id).ConfigureAwait(false);
+            return Ok(value);
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(
+                new Value {Id = 1, Name = "Value1"},
+                new Value {Id = 2, Name = "Value2"},
+                new Value {Id = 3, Name = "Value3"}
+            );
+        }
+    }
+}
