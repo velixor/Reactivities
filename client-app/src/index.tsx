cd2878a3474cebbc2b4d36eecef113d7a0e30b3e
Pavel Dushkov <velixor.work@gmail.com>
2020-07-27 01:04:36 +0300

    end of 106

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,10 +4,13 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import ScrollToTop from "./app/layout/ScrollToTop";
 
 ReactDOM.render(
     <BrowserRouter>
-        <App/>
+        <ScrollToTop>
+            <App/>
+        </ScrollToTop>
     </BrowserRouter>
     , document.getElementById('root'));
 
