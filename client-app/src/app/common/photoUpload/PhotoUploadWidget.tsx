4f20f8eb6924ce035c3cf2c69f67303cb9d59134
Pavel Dushkov <velixor.work@gmail.com>
2020-11-11 14:33:18 +0200

    end of 238

--- client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
@@ -2,16 +2,23 @@ import React, {Fragment, useEffect, useState} from 'react';
 import {Grid, Header, Image} from 'semantic-ui-react';
 import {observer} from 'mobx-react-lite';
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
 
 const PhotoUploadWidget = () => {
     const [files, setFiles] = useState<any[]>([]);
+    const [image, setImage] = useState<Blob | null>(null);
 
     useEffect(() => {
         return () => {
             files.forEach((file) => URL.revokeObjectURL(file.preview));
         };
     });
+
+    const getFile = (): any | null => {
+        if (files.length == 0) return null;
+        return files[0];
+    }
 
     return (
         <Fragment>
@@ -23,15 +30,16 @@ const PhotoUploadWidget = () => {
                 <Grid.Column width={1}/>
                 <Grid.Column width={4}>
                     <Header sub color='teal' content='Step 2 - Resize image'/>
+                    {getFile() && <PhotoWidgetCropper setImage={setImage} imagePreview={getFile().preview}/>}
                 </Grid.Column>
                 <Grid.Column width={1}/>
                 <Grid.Column width={4}>
                     <Header sub color='teal' content='Step 3 - Preview & Upload'/>
-                    {files.length > 0 && <Image src={files[0].preview}/>}
+                    {getFile() &&
+                    <div className='img-preview' style={{minHeight: '200px', overflow: 'hidden'}}/>}
                 </Grid.Column>
             </Grid>
         </Fragment>
-
     );
 };
 
