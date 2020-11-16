ee31ba3a85db47d1321354a1e64359f148f15c73
Pavel Dushkov <velixor.work@gmail.com>
2020-11-16 10:49:09 +0200

    end of 253

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,3 +1,12 @@
+interface IComment {
+    id: string;
+    createdAt: Date;
+    body: string;
+    userName: string;
+    displayName: string;
+    image: string;
+}
+
 export interface IActivity {
     id: string;
     title: string;
@@ -9,6 +18,7 @@ export interface IActivity {
     isGoing: boolean;
     isHost: boolean;
     attendees: IAttendee[]
+    comments: IComment[]
 }
 
 export interface IActivityFormValues extends Partial<IActivity> {
@@ -28,7 +38,7 @@ export class ActivityFormValues implements IActivityFormValues {
     constructor(init?: IActivityFormValues) {
         if (init && init.date) {
             init.time = init.date;
-        }  
+        }
         Object.assign(this, init);
     }
 }
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,12 @@
-import {observable, action, computed, runInAction} from 'mobx';
+import {action, computed, observable, runInAction} from 'mobx';
 import {SyntheticEvent} from 'react';
 import {IActivity} from '../models/activity';
 import agent from '../api/agent';
 import {history} from '../..';
 import {toast} from 'react-toastify';
 import {RootStore} from './rootStore';
-import {setActivityProps, createAttendee} from '../common/util/util';
+import {createAttendee, setActivityProps} from '../common/util/util';
+import {HubConnection, HubConnectionBuilder, LogLevel} from "@microsoft/signalr";
 
 export default class ActivityStore {
     rootStore: RootStore;
@@ -20,6 +21,29 @@ export default class ActivityStore {
     @observable submitting = false;
     @observable target = '';
     @observable loading = false;
+    @observable.ref hubConnection: HubConnection | null = null;
+
+    @action createHubConnection = () => {
+        this.hubConnection = new HubConnectionBuilder()
+            .withUrl('http://localhost:5000/chat', {
+                accessTokenFactory: () => this.rootStore.commonStore.token!
+            })
+            .configureLogging(LogLevel.Information)
+            .build();
+
+        this.hubConnection
+            .start()
+            .then(() => console.log(this.hubConnection!.state))
+            .catch(error => console.log('Error establishing connection: ', error));
+
+        this.hubConnection.on('ReceiveComment', comment => {
+            this.activity!.comments.push(comment);
+        })
+    }
+
+    @action stopHubConnection = () => {
+        this.hubConnection!.stop();
+    }
 
     @computed get activitiesByDate() {
         return this.groupActivitiesByDate(
